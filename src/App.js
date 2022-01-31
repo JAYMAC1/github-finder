07bb808185c31072d4df152b349329f70bcea944
James McNeil <mcneil_james@hotmail.com>
2022-01-31 12:00:50 +0000

    added routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
 
 import Navbar from './components/layout/Navbar'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 function App() {
   return (
     <BrowserRouter>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main className='container mx-auto px-3 pb-12'>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/notfound' element0={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />{' '}
+          </Routes>
+        </main>
         <Footer />
       </div>
     </BrowserRouter>
