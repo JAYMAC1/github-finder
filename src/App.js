2720baf89c898f79ec803242a46b89b578592440
James McNeil <mcneil_james@hotmail.com>
2022-01-31 17:01:43 +0000

    removed Link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // react components
-import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Footer from './components/layout/Footer'
+
+// context and hooks
+import { GithubProvider } from './context/github/GithubContext'
 
 import Navbar from './components/layout/Navbar'
 import Home from './pages/Home'
@@ -8,20 +11,22 @@ import About from './pages/About'
 import NotFound from './pages/NotFound'
 function App() {
   return (
-    <BrowserRouter>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/notfound' element0={<NotFound />} />
-            <Route path='/*' element={<NotFound />} />{' '}
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <GithubProvider>
+      <BrowserRouter>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='/notfound' element0={<NotFound />} />
+              <Route path='/*' element={<NotFound />} />{' '}
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </GithubProvider>
   )
 }
 
