c09d9df1e186a07ffd89df1f6466ffb75f5d17bd
James McNeil <mcneil_james@hotmail.com>
2022-01-31 17:22:55 +0000

    added searchbar and button

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UserSearch = () => {
-  return <div>Search</div>
+  const [search, setSearch] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (search.trim() === '') {
+      alert('please enter a search term')
+    } else {
+      console.log(search)
+      setSearch('')
+    }
+  }
+  return (
+    <div className='grid  grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8'>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <div className='form-control'></div>
+          <div className='relative'>
+            <input
+              type='text'
+              className='w-full pr-40 bg-gray-200 input input-lg text-black'
+              placeholder='Search'
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+            <button
+              className='absolute top-0 right-0 rounded-l-none w-36 btn btn-lg'
+              type='submit'>
+              Go
+            </button>
+          </div>
+        </form>
+      </div>
+      <div>
+        <button className='btn btn-ghost btn-gl' onClick={() => setSearch('')}>
+          Clear
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default UserSearch
