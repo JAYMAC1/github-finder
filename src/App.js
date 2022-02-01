a4ba1c935741f2cc208009e3f230789434664a81
James McNeil <mcneil_james@hotmail.com>
2022-02-01 14:19:53 +0000

    imported AlertContext and wrapped the provder around components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Footer from './components/layout/Footer'
 
 // context and hooks
 import { GithubProvider } from './context/github/GithubContext'
+import AlertContext, { AlertProvider } from './context/alret/AlertContext'
 
+//  Components and Pages
 import Navbar from './components/layout/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
@@ -12,20 +14,22 @@ import NotFound from './pages/NotFound'
 function App() {
   return (
     <GithubProvider>
-      <BrowserRouter>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element0={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />{' '}
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </BrowserRouter>
+      <AlertProvider>
+        <BrowserRouter>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element0={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />{' '}
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </AlertProvider>
     </GithubProvider>
   )
 }
