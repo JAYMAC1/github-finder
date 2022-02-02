c95680b45accc3255a90d034ad916f4a9d66044c
James McNeil <mcneil_james@hotmail.com>
2022-02-02 13:39:11 +0000

    initial creation of Users component

--- src/components/users/User.jsx
+++ src/components/users/User.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import GithubContext from '../../context/github/GithubContext'
 
 const User = () => {
-  return <div>User</div>
+  const { getUser, user } = useContext(GithubContext)
+
+  console.log(user)
+
+  const params = useParams()
+
+  useEffect(() => {
+    getUser(params.gitUser)
+  }, [params.gitUser])
+
+  return <div>{user.login}</div>
 }
 
 export default User
